e379295f6d3dd8193f30a00ca32c7dca1ffd37e1
Viktory95 <cherkasovavika@mail.ru>
2024-12-25 21:03:00 +0300

    Initial commit.

---
+++ src/main/java/com/vi/StoryHelperAuth/StoryHelperAuthApplication.java
@@ -0,0 +1,13 @@
+package com.vi.StoryHelperAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryHelperAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryHelperAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vi/StoryHelperAuth/StoryHelperAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vi.StoryHelperAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryHelperAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
